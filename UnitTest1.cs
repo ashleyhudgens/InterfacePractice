8cfbe7fd642c711f95997a669f09ab9194e30efd
ashleyhudgens <39204522+ashleyhudgens@users.noreply.github.com>
2018-09-19 17:00:49 -0500

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using TestingInterfaces;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodAdd()
+        {
+            //arrange
+            var result = new Arithmetic();
+            //act
+            result.typeOfArithmetic = "add";
+            result.firstOperand = 1;
+            result.secondOperand = 2;
+            result.getDisplayTotal();
+            //assert
+            Assert.AreEqual(result.total, 3);
+        }
+
+        [TestMethod]
+        public void TestMethodSubtract()
+        {
+            //arrange
+            var result = new Arithmetic();
+            //act
+            result.typeOfArithmetic = "subtract";
+            result.firstOperand = 10;
+            result.secondOperand = 2;
+            result.getDisplayTotal();
+            //assert
+            Assert.AreEqual(result.total, 8);
+        }
+
+        [TestMethod]
+        public void TestMethodMultiply()
+        {
+            //arrange
+            var result = new Arithmetic();
+            //act
+            result.typeOfArithmetic = "multiply";
+            result.firstOperand = 6;
+            result.secondOperand = 6;
+            result.getDisplayTotal();
+            //assert
+            Assert.AreEqual(result.total, 36);
+        }
+
+        [TestMethod]
+        public void TestMethodDivide()
+        {
+            //arrange
+            var result = new Arithmetic();
+            //act
+            result.typeOfArithmetic = "divide";
+            result.firstOperand = 50;
+            result.secondOperand = 5;
+            result.getDisplayTotal();
+            //assert
+            Assert.AreEqual(result.total, 10);
+        }
+    }
+}
