d0539ca2333397cf30a4de726b123b5ab645978c
ashleyhudgens <39204522+ashleyhudgens@users.noreply.github.com>
2018-09-19 16:59:58 -0500

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TestingInterfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var dsfa = new getTypeOfArithmetic();
+
+
+        }
+    }
+}
